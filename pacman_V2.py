db3bfa1387d7aa3d43829024c63b39f2795818e1
ISN-BLW <pingris.benoit665@gmail.com>
2016-04-05 22:32:28 +0200

    Update pacman_V2.py

--- pacman_V2.py
+++ pacman_V2.py
@@ -1,5 +1,7 @@
 from tkinter import *
-import cartePM
+import cartePM  #on importe un autre fichier python contenant les cartes.
+import random
+
 
 #Position de Blinky (fantôme rouge)
 posX1 = 24
@@ -18,6 +20,43 @@ dy = 0
 
 """L E S  F O N C T I O N S"""
 
+def update_fantomes():
+    global posX1, posY1, posX2, posY2
+    #On génére un nombre entre 1 et 4 pour faire varier posX1 et posY1
+    a = random.randint(1,4)
+    if a == 1:
+        posX1 += 1
+
+    elif a == 2:
+        posX1 -= 1
+
+    elif a== 3:
+        posY1 += 1
+        
+    else:
+        posY1 -= 1
+
+    #Meme chose pour posX2 et posY2
+    b = random.randint(1,4)
+    if b == 1:
+        posX2 += 1
+
+    elif b ==2:
+        posX2 -= 1
+        
+    elif b== 3:
+        posY2 += 1
+        
+    else:
+        posY2 -= 1
+
+            
+            
+    #On actualise les coordonnées des fantômes
+    canvasJ.coords(blinky, posX1*10-9, posY1*10-9, posX1*10+19, posY1*10+19)
+    canvasJ.coords(pinky, posX2*10-9, posY2*10-9, posX2*10+19, posY2*10+19)
+
+    canvasJ.after(200, update_fantomes)
 #Fonction de déplacement gérant les collisions avec murs + celles avec les pastilles, ainsi que les déplacements du pacman
 def depl(event, pacman, carteJeu):
     
@@ -48,48 +87,26 @@ def depl(event, pacman, carteJeu):
     if carteJeu[posY+2*dy][posX+2*dx] == 0 and carteJeu[posY+2*dy+abs(dx)][posX+2*dx+abs(dy)] == 0 and carteJeu[posY+2*dy-abs(dx)][posX+2*dx-abs(dy)] == 0: # case vide 
             posY += dy
             posX += dx
-    elif carteJeu[posY+2*dy][posX+2*dx] == 8: # changement de coté
+    elif carteJeu[posY+2*dy][posX+2*dx] == 8:  #Changement de coté
         if dx == 1:
             posX = 3
         elif dx == -1:
             posX = 55
-    elif carteJeu[posY+2*dy][posX+2*dx] == 9: # La case est occupée par une pastille
-        carteJeu[posY+2*dy][posX+2*dx] = 0 # On mange la pastille, la case devient vide
+    elif carteJeu[posY+2*dy][posX+2*dx] == 9:  #La case est occupée par une pastille
+        carteJeu[posY+2*dy][posX+2*dx] = 0  #On mange la pastille, la case devient vide
         
-        canvasJ.addtag_closest('miam', (posX+2*dx)*10+5,(posY+2*dy)*10+5, halo=5) # On tag l'objet 'pastille' sur le canvas -> il s'appelle 'miam'
-        canvasJ.delete('miam') # On supprime la pastille du canvas
+        canvasJ.addtag_closest('miam', (posX+2*dx)*10+5,(posY+2*dy)*10+5, halo=5)  #On tag l'objet 'pastille' sur le canvas -> il s'appelle 'miam'
+        canvasJ.delete('miam')  #On supprime la pastille du canvas
         posY += dy
         posX += dx
 
     canvasJ.create_arc(posX*10-9, posY*10-9 ,posX*10+19 ,posY*10+19, fill = "yellow", start = (dy*90+abs(dy)*225)+(dx*90-abs(dx)*45), extent = 270) # on re affiche le pacman
 
-   
-"""        
-def deplacement(event, canvas, pacman):
-    global posX, posY
-    touche = event.keysym
-    print(touche)
-    
-    if touche == "Up":
-        posY -=1
-        canvas.itemconfig(pacman, start = 135, extent = 270)
-    elif touche == "Down":
-        posY += 1
-        canvas.itemconfig(pacman, start = -45, extent = 270)
-    elif touche == "Right":
-        posX +=1
-        canvas.itemconfig(pacman, start = 45, extent = 270)
-    elif touche == "Left":
-        posX -=1
-        canvas.itemconfig(pacman, start = 225, extent = 270)
-
-    canvas.coords(pacman,posX*10-9, posY*10-9 ,posX*10+19 ,posY*10+19)
-"""
 
     
 def makeMap(carteJeu):
 
-    
+    #Créer la carte choisie avec 2 boucles for à partir d'une liste de liste
     gameMenu_frame.grid_forget()
     game_frame.grid()
     
@@ -119,14 +136,19 @@ def show_rules():
     rules_frame.grid()
  
 def show_menu():
+    #On réinitialise les positions du pacman à chaque fois que cette fonction est appelée
+    #Ainsi à chaque fois que l'utilisateur est en jeu et qu'il retourne à la fenêtre principale alors les positions se réinitialisent toutes et un nouvelle partie peut débuter
     global posX, posY, posX1, posY1, posX2, posY2
-    posX2 = 34
-    posY2 = 32
-    posX1 = 24
-    posY1 = 32
     posX = 29
     posY = 48
-    canvasJ.coords(pacman,posX*10-9, posY*10-9 ,posX*10+19 ,posY*10+19)
+    posX1 = 24
+    posY1 = 32
+    posX2 = 34
+    posY2 = 32
+    canvasJ.delete("all") #On supprime tous les objets : fantômes et pacman
+    
+    blinky = canvasJ.create_oval(posX1*10-9, posY1*10-9,posX1*10+19,posY1*10+19, fill = "red")
+    pinky = canvasJ.create_oval(posX2*10-9, posY2*10-9, posX2*10+19, posY2*10+19, fill = "pink")
     for child in app.winfo_children():
         child.grid_forget()
     menu_frame.grid()
@@ -153,25 +175,35 @@ quitterB.grid(row = 5, column = 5, ipady = 10)
 
  
 """C H O I X  D E S  C A R T E S"""
+#Frame pour le menu
 gameMenu_frame = Frame(app, bg = "#172457")
+
+#Bouton pour retourner au menu
 return_btn = Button(gameMenu_frame, text='Retour au menu', command=show_menu)
 return_btn.grid(row = 1, column = 3, ipady = 10)
 
+#Bouton pour choisir la carte 1
 bouCarte1 = Button(gameMenu_frame, text = "Carte 1", bd = 4, bg = "#BEBE15",  command = lambda carte1=cartePM.carte1: makeMap(cartePM.carte1))
 bouCarte1.grid(pady = 10,ipady = 5, padx = 5,  row = 1, column = 1)
-      
+
+#Bouton pour choisir la carte 2
 bouCarte2 = Button(gameMenu_frame, text = "Carte 2",bd = 4, bg = "#BEBE15",  command = lambda carte1=cartePM.carte2: makeMap(cartePM.carte2))
 bouCarte2.grid(pady = 10, ipady = 5, padx = 5, row = 1, column = 2)
 
 
+
 """L E J E U"""
 game_frame = Frame(app)
+
 canvasJ = Canvas(game_frame, bg = "dark blue", width = 600, height = 650)
 canvasJ.pack(side = LEFT)
 canvasJ.focus_set()
 canvasJ.bind("<Key>", lambda event: depl(event, pacman, cartePM.carte2))
+
+#Bouton pour retourner au menu
 return_btn = Button(game_frame, text='Retour au menu', command=show_menu, relief = "ridge")
 return_btn.pack(ipady = 10, padx = 5)
+
 #Création du pac man
 pacman = canvasJ.create_arc(posX*10-9, posY*10-9 ,posX*10+19 ,posY*10+19, fill = "#BEBE15", start = 225, extent = 270)
     
@@ -183,6 +215,7 @@ pinky = canvasJ.create_oval(posX2*10-9, posY2*10-9, posX2*10+19, posY2*10+19, fi
 
 
 
+canvasJ.after(200, update_fantomes)
 
 
 """R E G L E S"""
