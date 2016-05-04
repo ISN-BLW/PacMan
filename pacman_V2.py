95dd5041953d938e36ebb9f75c08ec8e5b2e18fe
ISN-BLW <pingris.benoit665@gmail.com>
2016-05-04 12:49:57 +0200

    Update pacman_V2.py

--- pacman_V2.py
+++ pacman_V2.py
@@ -7,12 +7,21 @@ import random   #On importe
 marche_fantome = False
 
 #Position de Blinky (fantôme rouge)
-posX1 = 24
-posY1 = 32
+posX1 = 44
+posY1 = 12
 
 #Position de Pinky (fantôme rose)
-posX2 = 34
-posY2 = 32
+posX2 = 44
+posY2 = 42
+
+#Position de Bleuy (fantôme bleu)
+posX3 = 14
+posY3 = 42
+
+#Position de Verty (fantôme vert)
+posX4 = 14
+posY4 = 12
+
 
 #Position de PacMan
 posX = 29
@@ -34,11 +43,14 @@ pointsScore = 0
 
 """L E S  F O N C T I O N S"""
 
+def bruh(event):
+    print ("clicked at", event.x, event.y)
+    fantome_True()
 #Cette fonction permet d'initialiser la fonction de déplacement des fantômes.
 def fantome_True():
     global marche_fantome
     marche_fantome = True
-    canvasJ.after(200, update_fantomes)
+    canvasJ.after(250, update_fantomes)
 
 
     
@@ -64,12 +76,14 @@ def update_fantomes():
         else:
             dx1 = 0
             dy1 = -1
+            
 
 
         if cartePM.carte2[posY1+2*dy1][posX1+2*dx1] == 0 and cartePM.carte2[posY1+2*dy1+abs(dx1)][posX1+2*dx2+abs(dy1)] == 0 and cartePM.carte2[posY1+2*dy1-abs(dx1)][posX1+2*dx1-abs(dy1)] == 0: # case vide 
             posY1 += dy1
             posX1 += dx1
         #Meme chose pour posX2 et posY2
+            
         b = random.randint(1,4)
         if b == 1:
             dx2 = 1
@@ -149,7 +163,7 @@ def depl(event, pacman, carteJeu):
         dy = 0
         canvasJ.itemconfig(pacman, start = 45, extent = 270)
 
-    canvasJ.coords(pacman,posX*10-9, posY*10-9 ,posX*10+19 ,posY*10+19)
+    
     
     #canvasJ.addtag_closest('pacman', posX*10,posY*10, halo=5) # On tag l'objet 'pacman' sur le canvas -> il s'appelle 'pacman'
     #canvasJ.delete('pacman')
@@ -157,6 +171,7 @@ def depl(event, pacman, carteJeu):
     if carteJeu[posY+2*dy][posX+2*dx] == 0 and carteJeu[posY+2*dy+abs(dx)][posX+2*dx+abs(dy)] == 0 and carteJeu[posY+2*dy-abs(dx)][posX+2*dx-abs(dy)] == 0: # case vide 
             posY += dy
             posX += dx
+    
     elif carteJeu[posY+2*dy][posX+2*dx] == 8:  #Changement de coté
         if dx == 1:
             posX = 3
@@ -171,15 +186,18 @@ def depl(event, pacman, carteJeu):
         canvasJ.delete('miam')  #On supprime la pastille du canvas
         posY += dy
         posX += dx
-
+        
+    canvasJ.coords(pacman,posX*10-9, posY*10-9 ,posX*10+19 ,posY*10+19)
+    
     #canvasJ.create_arc(posX*10-9, posY*10-9 ,posX*10+19 ,posY*10+19, fill = "yellow", start = (dy*90+abs(dy)*225)+(dx*90-abs(dx)*45), extent = 270) # on re affiche le pacman
 
     if posX == posX2 and posY == posY2:
         posY = 48
         posX = 29
         pointsScore -= 200
+        lbl_Score.config(text = "P.O.I.N.T.S\n" + str(pointsScore))
         canvasJ.coords(pacman,posX*10-9, posY*10-9 ,posX*10+19 ,posY*10+19)
-    if pointsScore == 2460: #246 points au total
+    if pointsScore == 2060: #246 points au total
        #on affiche un message pour alerter le joueur de sa victoire et on le fait revenir au menu du jeu
         showinfo("ALERTE", "Vous avez gagné !")
         show_menu()
@@ -187,10 +205,11 @@ def depl(event, pacman, carteJeu):
 
  #Fonction pour créer la carte   
 def makeMap(carteJeu):
-    fantome_True()
+    
     #Créer la carte choisie avec 2 boucles for à partir d'une liste de liste
     gameMenu_frame.grid_forget()
     game_frame.grid()
+    
     
     for y in range(65):
         for x in range(59):
@@ -200,13 +219,14 @@ def makeMap(carteJeu):
 
             elif carteJeu[y][x]==9:
                 canvasJ.create_oval(x*10+3, y*10+3, x*10+7, y*10+7, fill = "yellow", outline = "")
-
+    
    #On remet au premier plan les objets animés (pacman, fantômes)
     canvasJ.tag_raise(pacman)
     canvasJ.tag_raise(blinky)
     canvasJ.tag_raise(pinky)
     
 def start_game():
+    
     # app (Tk) contient trois enfants : nos 3 frames.
     # On appelle grid_forget sur les 3, puis on
     # appelle grid sur celle qu'on veut montrer
@@ -227,10 +247,10 @@ def show_menu():
     marche_fantome = False
     posX = 29
     posY = 48
-    posX1 = 24
-    posY1 = 32
-    posX2 = 34
-    posY2 = 32
+    posX1 = 44
+    posY1 = 12
+    posX2 = 44
+    posY2 = 42
     pointsScore = 0
 
     lbl_Score.config(text = "P.O.I.N.T.S\n" + str(pointsScore))
@@ -275,8 +295,8 @@ quitterB.pack(ipady = 15, ipadx = 15, pady = 15, padx = 15)
  
 """C H O I X  D E  L A  C A R T E"""
 #Frame pour le menu
-gameMenu_frame = Frame(app, bg = "#2EA4BA ")
-boxGM = Frame(gameMenu_frame, bg = "#017F97 ")
+gameMenu_frame = Frame(app, bg = "#2EA4BA")
+boxGM = Frame(gameMenu_frame, bg = "#017F97")
 boxGM.pack(padx = 300, pady = 150)
 
 #Bouton pour choisir la carte 
@@ -318,7 +338,10 @@ pinky = canvasJ.create_oval(posX2*10-9, posY2*10-9, posX2*10+19, posY2*10+19, fi
 
 #Label affichant le score
 lbl_Score = Label(game_frame, text = "P.O.I.N.T.S\n" + str(pointsScore), font = "Courier 20 bold", bg = "#017F97", fg = "white")
-lbl_Score.pack(pady = 50, padx = 10)                         
+lbl_Score.pack(pady = 50, padx = 10)
+
+lbl_pos = Label(game_frame)
+canvasJ.bind("<Button-1>", bruh)
 
 if pointsScore == 20:
     print("Wonja va récuperer ces bananes")
